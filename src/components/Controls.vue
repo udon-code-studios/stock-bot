7af04f6db9b3099759044f79674f75c595915c60
Leo <rocketthedogliu@gmail.com>
2020-12-23 22:15:16 +0000

    testing vue

---
+++ src/components/Controls.vue
@@ -0,0 +1,18 @@
+<template>
+    <form>
+        <label>Symbol: <input id="symbol" required></label>
+        <input type="submit" value="Submit">
+    </form>
+</template>
+
+<script lang=ts>
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Controls extends Vue {
+  
+}
+</script>
+
+<style scoped>
+</style>
